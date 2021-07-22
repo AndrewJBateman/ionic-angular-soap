18cf268965cb92a6cb07175d7eacd4f684f9078a
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-22 16:29:15 +0200

    feat: show country data from data service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  countries = [];
+  constructor(private dataService: DataService, private loadingController: LoadingController) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.dataService.clientState().subscribe((ready) => {
+      if (ready) {
+        this.loadAllCountries();
+      }
+    });
+  }
 
+  async loadAllCountries() {
+    const loading = await this.loadingController.create({
+      message: 'loading countries'
+    });
+    await loading.present();
+
+    this.dataService.getAllCountries().subscribe((countries) => {
+      this.countries = countries;
+      loading.dismiss();
+    });
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Client, NgxSoapService } from 'ngx-soap';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  client: Client;
+  private clientReady = new BehaviorSubject(false);
+  constructor(private soap: NgxSoapService) {
+    this.soap.createClient('./assets/CountryInfoService.xml').then((client) => {
+      console.log('client: ', client);
+      this.client = client;
+      this.clientReady.next(true);
+    });
+  }
 
-  constructor() { }
+  clientState() {
+    return this.clientReady.asObservable();
+  }
+
+  getAllCountries() {
+    return this.client.call('ListOfCountryNamesByName', {}).pipe(
+      map((data) => data.result.ListOfCountryNamesByNameResult.tCountryCodeAndName)
+    );
+  }
+
+  getFlagForCountry(isocde) {
+    return this.client.call('CountryFlag', {sCountryISOCode: isocde}).pipe(
+      map((data) => data.result.CountryFlagResult)
+    );
+  }
+
+  getCurrencyForCountry(isocde) {
+    return this.client.call('CountryCurrency', {sCountryISOCode: isocde}).pipe(
+      map((data) => data.result.CountryCurrencyResult)
+    );
+  }
+
+  getCapitalForCountry(isocde) {
+    return this.client.call('CapitalCity', {sCountryISOCode: isocde}).pipe(
+      map((data) => data.result.CapitalCityResult)
+    );
+  }
+
 }
