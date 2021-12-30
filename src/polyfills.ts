8a77b718dea20c473b5ccd1c854cdaa155f296f5
AndrewJBateman <gomezbateman@yahoo.com>
2021-12-30 12:42:33 +0100

    fix: ensure app passes es linting

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,4 +58,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
   env: { DEBUG: undefined, PRODUCTION: undefined },
 };
 
-(window as any)['global'] = window;
+(window as any).global = window;
